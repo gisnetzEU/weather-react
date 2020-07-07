940166bb1ff1e2e29134c7ea957f9ba4a619a9c8
gisnetz <gisnetz@msn.com>
2020-07-07 14:39:02 +0200

    Updates

---
+++ precache-manifest.2d691b46772c00166769c8c778258b30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73a09a502fd2d38e38d54f8c4d7f3674",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "98c6fa701b0c7608f789",
+    "url": "/weather-react/static/css/main.de4b8acf.chunk.css"
+  },
+  {
+    "revision": "ea00e207e7033c38a445",
+    "url": "/weather-react/static/js/2.a5ddeacf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react/static/js/2.a5ddeacf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98c6fa701b0c7608f789",
+    "url": "/weather-react/static/js/main.bcbbca84.chunk.js"
+  },
+  {
+    "revision": "978dc9f2a9f9512ae584",
+    "url": "/weather-react/static/js/runtime-main.17cf43c5.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/weather-react/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/weather-react/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
